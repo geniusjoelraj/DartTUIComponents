434f8edabe7c18bd8d878193632ee68e561c0b43
geniusjoelraj <geniusjoel2005@gmail.com>
2025-03-24 18:31:00 +0530

    checkboxes implemented

--- bin/sample_project.dart
+++ bin/sample_project.dart
@@ -1,6 +1,6 @@
 import 'package:sample_project/menu.dart';
 
 void main() {
-  var menu = Menu(["Dart", "TUI", "Menu", 10, "Exit"]);
+  var menu = Menu(["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]);
   menu.displayMenu();
 }
--- lib/menu.dart
+++ lib/menu.dart
@@ -7,28 +7,43 @@ class Menu {
   List<dynamic> menuItems;
 
   Menu(this.menuItems);
+  Map<String, bool> checked = <String, bool>{};
 
   /// Display menu items
   void displayMenu() {
-    int selectedIndex = 0;
+    for (var item in menuItems) {
+      checked[item] = false;
+    }
 
+    int selectedIndex = 0;
+    console.hideCursor();
     while (true) {
       console.clearScreen();
       console.resetCursorPosition();
-      console.writeLine("Use arrow keys to navigate. Press Enter to select.\n");
+      console.writeLine(
+        "Use arrow keys to navigate. Press SpaceBar to check or uncheck.\n",
+      );
 
       // Display menu items
       for (int i = 0; i < menuItems.length; i++) {
         if (i == selectedIndex) {
           console.setForegroundColor(ConsoleColor.green);
-          console.writeLine("> ${menuItems[i]}");
+          if (checked[menuItems[i]] == true) {
+            console.writeLine("> [x] ${menuItems[i]}");
+          } else {
+            console.writeLine("> [ ] ${menuItems[i]}");
+          }
           console.resetColorAttributes();
         } else {
-          console.writeLine("  ${menuItems[i]}");
+          if (checked[menuItems[i]] == true) {
+            console.writeLine("  [x] ${menuItems[i]}");
+          } else {
+            console.writeLine("  [ ] ${menuItems[i]}");
+          }
         }
       }
 
-      // Capture user input
+      // Reading input
       var key = console.readKey();
 
       if (key.controlChar == ControlCharacter.arrowUp) {
@@ -39,13 +54,11 @@ class Menu {
         if (selectedIndex != menuItems.length - 1) {
           selectedIndex += 1;
         }
-      } else if (key.controlChar == ControlCharacter.enter) {
+      } else if (key.char == ' ') {
+        checked[menuItems[selectedIndex]] = !checked[menuItems[selectedIndex]]!;
+      } else if (key.char == 'q') {
         console.clearScreen();
-        console.writeLine("You have selected ${menuItems[selectedIndex]}");
-        if (menuItems[selectedIndex] == "Exit") {
-          exit(0);
-        }
-        sleep(Duration(seconds: 1));
+        exit(0);
       }
     }
   }
